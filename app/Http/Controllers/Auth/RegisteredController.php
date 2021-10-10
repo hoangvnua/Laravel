9c057743ba3ae75331aad3070794748c8d92906c
hoangvnua <nvhoang.vnua@gmail.com>
2021-10-10 21:56:18 +0700

    tao tai khoan la admin

--- app/Http/Controllers/Auth/RegisteredController.php
+++ app/Http/Controllers/Auth/RegisteredController.php
@@ -31,7 +31,8 @@ class RegisteredController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'status' => 1
+            'status' => 1,
+            'role' => 'admin'
         ]);
 
         DB::table('user_infos')->insert([
