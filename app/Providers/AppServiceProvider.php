fe771dc9cdc36b35d37d004e3e35aa80aeb8040e
hoangvnua <75560659+hoangvnua@users.noreply.github.com>
2023-07-01 15:13:43 +0700

    Add files via upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Models\Menu;
 use App\Models\SubMenu;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -32,16 +31,13 @@ class AppServiceProvider extends ServiceProvider
         // $menu = ['Phone', 'Laptop'];
         // view()->share('menu', $menu);
 
-        // $menus = Menu::get();
-        // $sub =  SubMenu::get();
-        // View::share([
-        //     'menus' => $menus,
-        //     'sub' => $sub
-        // ]);
+        $menus = Menu::get();
+        $sub =  SubMenu::get();
+        View::share([
+            'menus' => $menus,
+            'sub' => $sub
+        ]);
 
         Paginator::useBootstrap();
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 }
